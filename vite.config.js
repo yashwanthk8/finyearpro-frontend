55fd37f1bd675ce549dc301178dc6e308c0236f6
YASHWANTH KUMAR <yashwanthkumar@YASHWANTHs-MacBook-Air.local>
2025-04-01 15:45:26 +0530

    Fix lottie-web eval issue in build process

--- vite.config.js
+++ vite.config.js
@@ -11,23 +11,44 @@ export default defineConfig({
       transformMixedEsModules: true
     },
     rollupOptions: {
-      external: [],
+      external: [
+        '@fortawesome/fontawesome-svg-core',
+        '@fortawesome/free-solid-svg-icons',
+        '@fortawesome/free-regular-svg-icons',
+        '@fortawesome/free-brands-svg-icons'
+      ],
       output: {
+        globals: {
+          '@fortawesome/fontawesome-svg-core': 'FontAwesomeCore',
+          '@fortawesome/free-solid-svg-icons': 'FontAwesomeSolid',
+          '@fortawesome/free-regular-svg-icons': 'FontAwesomeRegular',
+          '@fortawesome/free-brands-svg-icons': 'FontAwesomeBrands'
+        },
         manualChunks: {
-          fontawesome: [
-            '@fortawesome/fontawesome-svg-core',
-            '@fortawesome/free-solid-svg-icons',
-            '@fortawesome/free-regular-svg-icons',
-            '@fortawesome/free-brands-svg-icons'
-          ],
           vendor: [
             'react', 
             'react-dom', 
             'react-router-dom'
-          ]
+          ],
+          lottie: ['lottie-web', 'lottie-react']
         }
       }
-    }
+    },
+    minify: 'terser',
+    terserOptions: {
+      parse: {
+        // Workaround for lottie-web eval usage
+        bare_returns: false
+      },
+      compress: {
+        // Disable eval warnings in console but allow them to work
+        passes: 2,
+        drop_console: false,
+        ecma: 2020
+      }
+    },
+    // Allow eval usage in lottie-web
+    sourcemap: false
   },
   resolve: {
     alias: {
